a95657374dfc046b17d688d49692b36025b45755
尚嘉宣 <shangjiaxuan@shangjiaxuandeMacBook-Pro.local>
2017-12-10 15:43:23 +0800

    deleted some

--- DerivedData/1010_shangjiaxuan/Build/Intermediates.noindex/1010_shangjiaxuan.build/Debug/1010_shangjiaxuan.build/Objects-normal/x86_64/Shape.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/shangjiaxuan/Desktop/北大/资料/大一上/计算概论/大作业/1010_shangjiaxuan/1010_shangjiaxuan/Shape.cpp \
-  /Users/shangjiaxuan/Desktop/北大/资料/大一上/计算概论/大作业/1010_shangjiaxuan/1010_shangjiaxuan/Shape.h
